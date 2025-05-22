18995db100f3adf31db5b5ab6e5621426e152ee0
Jiraporn1008 <jiraporn.wang@mail.kmutt.ac.th>
2025-05-22 10:58:40 +0700

    refactor

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from werkzeug.utils import secure_filename
 import pandas as pd
 from io import BytesIO
+import os
 
 app = Flask(__name__)
 
@@ -45,5 +46,6 @@ def upload():
 
     return jsonify(mapped_data)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
